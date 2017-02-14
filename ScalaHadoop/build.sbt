1aa0d5076b150bb5d18e68213801939728dfc31e
gkatzioura@gmail.com <gkatzioura@gmail.com>
2017-02-14 08:39:30 +0200

    Added .gitignore and wordcount object

---
+++ ScalaHadoop/build.sbt
@@ -0,0 +1,8 @@
+name := "ScalaHadoop"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+
+//resolvers += "cloudera" at "https://repository.cloudera.com/artifactory/cloudera-repos/"
+libraryDependencies += "org.apache.hadoop" % "hadoop-mapreduce-client-core" % "2.7.1"
---
+++ ScalaHadoop/src/main/scala/com/gkatzioura/scala/hadoop/WordCount.scala
@@ -0,0 +1,25 @@
+package com.gkatzioura.scala
+
+
+/**
+  * Created by gkatzioura on 2/14/17.
+  */
+package object WordCount {
+
+
+  def main(args: Array[String]): Unit = {
+//    val configuration = new Configuration()
+//    Job job = Job.getInstance(conf, "word count");
+//    job.setJarByClass(WordCount.class);
+//    job.setMapperClass(TokenizerMapper.class);
+//    job.setCombinerClass(IntSumReducer.class);
+//    job.setReducerClass(IntSumReducer.class);
+//    job.setOutputKeyClass(Text.class);
+//    job.setOutputValueClass(IntWritable.class);
+//    FileInputFormat.addInputPath(job, new Path(args[0]));
+//    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//    System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+  }
+
+}
