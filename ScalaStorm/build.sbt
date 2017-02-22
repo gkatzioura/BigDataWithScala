87d8f6d51c28ea68635c13606be964c9ed91c4ee
gkatzioura <gkatzioura@gmail.com>
2017-02-22 09:07:15 +0200

    Added assembly plugin

--- ScalaStorm/build.sbt
+++ ScalaStorm/build.sbt
@@ -6,4 +6,4 @@ scalaVersion := "2.12.1"
 
 scalacOptions += "-Yresolve-term-conflict:package"
 
-libraryDependencies += "org.apache.storm" % "storm-core" % "1.0.2"
+libraryDependencies += "org.apache.storm" % "storm-core" % "1.0.2" % "provided"
--- ScalaStorm/src/main/scala/com/gkatzioura/scala/storm/WordCountTopology.scala
+++ ScalaStorm/src/main/scala/com/gkatzioura/scala/storm/WordCountTopology.scala
@@ -7,26 +7,28 @@ import org.apache.storm.tuple.Fields
 /**
   * Created by gkatzioura on 2/18/17.
   */
-object WordCountTopology extends App {
+object WordCountTopology  {
 
-  val builder = new TopologyBuilder
-  builder.setSpout("spout",new RandomSentenceSpout,5)
-  builder.setBolt("split", new SplitSentenceBolt, 8).shuffleGrouping("spout")
-  builder.setBolt("count", new WordCountBolt,12).fieldsGrouping("split",new Fields("word"))
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+    val builder = new TopologyBuilder
+    builder.setSpout("spout", new RandomSentenceSpout, 5)
+    builder.setBolt("split", new SplitSentenceBolt, 8).shuffleGrouping("spout")
+    builder.setBolt("count", new WordCountBolt, 12).fieldsGrouping("split", new Fields("word"))
 
-  val conf = new Config()
-  conf.setDebug(true)
+    val conf = new Config()
+    conf.setDebug(true)
 
-  if (args != null && args.length > 0) {
-    conf.setNumWorkers(3)
-    StormSubmitter.submitTopology(args(0), conf, builder.createTopology())
+    if (args != null && args.length > 0) {
+      conf.setNumWorkers(3)
+      StormSubmitter.submitTopology(args(0), conf, builder.createTopology())
+    }
+    else {
+      conf.setMaxTaskParallelism(3)
+      val cluster = new LocalCluster
+      cluster.submitTopology("word-count", conf, builder.createTopology())
+      Thread.sleep(10000)
+      cluster.shutdown()
+    }
   }
-  else {
-    conf.setMaxTaskParallelism(3)
-    val cluster = new LocalCluster
-    cluster.submitTopology("word-count", conf, builder.createTopology())
-    Thread.sleep(10000)
-    cluster.shutdown()
-  }
-
 }
